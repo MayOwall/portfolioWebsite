c1b083fbe7caf3242bb2987d66ca817097b05404
Owall <97934878+MayOwall@users.noreply.github.com>
2022-10-17 22:32:53 +0900

    modify : NextJS 설정 수정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -8,8 +8,7 @@ const globalStyles = css`
         overflow: hidden;
         padding: 0;
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        font-family: "Noto Serif KR", serif;
     }
     a {
         color: inherit;
