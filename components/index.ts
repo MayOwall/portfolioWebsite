314f6d23bec0b6e901545a3ce9ba5d7132e7aa9e
Owall <97934878+MayOwall@users.noreply.github.com>
2022-10-18 17:23:24 +0900

    feat : LoadingCircle 컴포넌트 및 스토리북 구현

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,9 @@
 import { OvalNav } from "./atoms/OvalNav";
+import { LoadingCircle } from "./atoms/LoadingCircle";
 
 import { MainNavBar } from "./features/MainNavBar";
 import { PageTopper } from "./features/PageTopper";
 
 import { MainTemplate } from "./templates/MainTemplate";
 
-export { OvalNav, MainNavBar, PageTopper, MainTemplate };
+export { OvalNav, MainNavBar, PageTopper, MainTemplate, LoadingCircle };
