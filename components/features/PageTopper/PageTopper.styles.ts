5dbb7ade5034913aeb66ddd7c51cbe6f0e226942
Owall <97934878+MayOwall@users.noreply.github.com>
2022-10-18 23:43:55 +0900

    feat : PageTopper 메인페이지로의 이동 기능 구현

--- components/features/PageTopper/PageTopper.styles.ts
+++ components/features/PageTopper/PageTopper.styles.ts
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
     display: flex;
+    position: relative;
     gap: 6rem;
     width: 100%;
 `;
@@ -13,6 +14,23 @@ export const Line = styled.div`
     border-top: 1px solid ${({ theme }) => theme.colors.darkSlateGreen};
 `;
 
+export const GoMain = styled.div`
+    display: flex;
+    align-items: flex-end;
+    gap: 0.3rem;
+    position: absolute;
+    top: 2rem;
+    left: 0;
+
+    color: ${({ theme }) => theme.colors.darkSlateGreen};
+    opacity: 50%;
+    transition: all 0.3s ease-in;
+    cursor: pointer;
+
+    :hover {
+        opacity: 100%;
+    }
+`;
 export const ChildrenContainer = styled.div`
     flex-shrink: 0;
 `;
